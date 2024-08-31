5ad2879bb60b7ec2562df91fd7c71df029950dd3
lenovo <alaaabdellatif22@gmail.com>
2024-08-31 09:49:01 +0300

    first load

--- src/test/java/Testingclients.java
+++ src/test/java/Testingclients.java
@@ -3,10 +3,7 @@ import AppAuth.logout;
 import clientmanagment.Addclient;
 import clientmanagment.Clientactions;
 import clientmanagment.Searchclient;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class Testingclients {
     @BeforeMethod
